d94ccf535dd87cb12acd45f9c868a0f32f86bba1
austinloucks <austin.loucks35@gmail.com>
2025-04-24 15:51:29 -0700

    Completed Express Server project

--- server/app.js
+++ server/app.js
@@ -1,10 +1,20 @@
 // import files and packages up here
-
+const express = require("express");
+const morgan = require("morgan");
+const data = require("./data.json");
 
 // create your express server below
-var app;
+var app = express();
 
 // add your routes and middleware below
+app.use(morgan("dev"));
+app.get("/", (req, res) => {
+  res.status(200).send("<h1>Top Spots Page</h1>");
+});
+
+app.get("/data", (req, res) => {
+  res.status(200).json(data);
+});
 
 // finally export the express application
 module.exports = app;
